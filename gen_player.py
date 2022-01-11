992031ad93ecd67a1f487d0d6dde7cf91f963ef2
Katemi-GitHub <thewords32@gmail.com>
2022-01-11 22:03:09 +0000

    Co-authored-by: ThatNormalTet <ThatNormalTet@users.noreply.github.com>

--- gen_player.py
+++ gen_player.py
@@ -2,6 +2,7 @@ import random
 import races
 import skill_tree
 import buff
+import json
 
 # Prototype 1
 class Player:
@@ -38,7 +39,7 @@ class Player:
         rc_buff = buff.rcb[self.race]
         self.hp = int(random.randint(50, 125) + ((health_multiplier * self.level) * rc_buff))
         arc_buff = buff.arcb[self.race]
-        self.arcanium = int(100 + ((self.level * level_multiplier) * arc_buff))
+        self.arkanius = int(100 + ((self.level * level_multiplier) * arc_buff))
         self.strength = random.randint(5, 50)
         if self.strength >= 40:
             self.skills.append('Berserker')
@@ -50,12 +51,12 @@ class Player:
         self.gift_1 = random.choice(skill_tree.gf)
         self.gift_2 = random.choice(skill_tree.gf)
         if self.gift_1 == self.gift_2:
-            self.final_gift = 'Extra ' + self.gift_1
+            self.final_gift = 'Great ' + self.gift_1
         else:
             self.final_gift = self.gift_1 + ' & ' + self.gift_2
 
     def stats(self):
-        print(f'HP: {self.hp}\nRace: {self.race}\nGift: {self.final_gift}\nSkills: {self.skills}\nLevel: {self.level}\nArcanium: {self.arcanium}\n')
+        print(f'HP: {self.hp}\nRace: {self.race}\nGift: {self.final_gift}\nSkills: {self.skills}\nLevel: {self.level}\nArkanius: {self.arkanius}\n')
 
 player = Player(120)
 player.stats()
@@ -72,6 +73,7 @@ while running:
             if len(command) == 4:
                 skill_tuple = (command[2], command[3])
                 command[2] = ' '.join(skill_tuple)
+            #-----------------------------------------------------------------
             for i in range(len(skill_tree.skill)):
                 if command[2] == skill_tree.skill[i]:
                     valid_skill = True
@@ -85,6 +87,7 @@ while running:
                     print('Error: This skill is already activated!\n')
             elif error == True:
                 print('Error: No skill found!\n')
+            #-----------------------------------------------------------------
 
     if command [0] == 'remove':
         if command[1] == 'skill':
@@ -105,6 +108,29 @@ while running:
                 print('Error: This skill is not activated!\n')
             elif len(player.skills) == 0:
                 print('Error: There are no skills left!\n')
+
+    if command[0] == 'save':
+        data = {
+            'Race' : player.race,
+            'Level' : player.level,
+            'HP' : player.hp,
+            'Arkanius' : player.arkanius,
+            'Intelligence' : player.intelligence,
+            'Strength' : player.strength,
+            'Gifts' : player.final_gift,
+            'Skills' : player.skills
+        }
+
+        with open('player_data.txt', 'w') as outfile:
+            json.dump(data, outfile)
     
+    if command[0] == 'erase':
+        with open('player_data.txt', 'w') as outfile:
+            json.dump('', outfile)
+
+    if command[0] == 'regen':
+        player = Player(120)
+        player.stats()
+            
     if command[0] == 'close':
         running = False
